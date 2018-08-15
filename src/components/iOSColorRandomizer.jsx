186e2f663cf5e0163a5f7b2994177657338777dc
escakot <ec@neorennie.com>
2018-08-15 16:31:02 -0400

    Added iOSColorRandomizer Page

---
+++ src/components/iOSColorRandomizer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ColorRandomizer extends Component {
+
+
+
+
+
+
+    render() {
+        return(
+            <div>
+                <h1>iOS Color Randomizer</h1>
+            </div>
+        )
+    }
+};
+export default ColorRandomizer;
